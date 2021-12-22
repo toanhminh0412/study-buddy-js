95505ccc8951ad94742deb4042fd8cb2f9ec4e46
Minh To <toanhminh0412@gmail.com>
2021-12-21 17:13:41 -0800

    clean up

--- server/index.js
+++ server/index.js
@@ -6,17 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Convert all object requested to json
+app.use(express.json())
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
