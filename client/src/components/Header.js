01fdb72f00e889c3167bc02084e8ac9eb6b266d5
Minh To <toanhminh0412@gmail.com>
2021-12-24 14:59:18 -0800

    Format Userprofile form

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,38 @@
 // import modules
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import {IoMdArrowDropdown} from "react-icons/io"
+import {IoMdArrowDropdown} from "react-icons/io";
+import {FiMenu} from "react-icons/fi";
 
 export default function Header() {
+
+
     return (
         <nav className="flex flex-row bg-red-500 lg:h-16 h-12 shadow-xl shadow-red-200">
-            <h1 className="ml-2 sm:ml-12 my-auto lg:text-3xl sm:text-xl font-medium text-white">StudyBuddy</h1>
-            <div className="ml-2 my-auto w-1/2 text-center 2xl:ml-40 lg:ml-20 sm:ml-4">
-                <Link to='/user-profile' className='ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white'>User Profile</Link>
-                <Link to='/' className='ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white'>Find a match</Link>
-                <Link to='/' className='ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white'>Message</Link>
+            <h1 className="ml-2 sm:ml-8 my-auto text-xl lg:text-3xl sm:text-xl font-medium text-white">StudyBuddy</h1>
+            <div className="ml-2 my-auto w-1/2 text-center 2xl:ml-40 lg:ml-16 sm:ml-2">
+                <Link to='/user-profile' className='hidden sm:inline-block ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white hover:text-amber-400'>User Profile</Link>
+                <Link to='/' className='hidden sm:inline-block ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white hover:text-amber-400'>Find a match</Link>
+                <Link to='/' className='hidden sm:inline-block ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white hover:text-amber-400'>Message</Link>
             </div>
-            <p className="ml-2 my-auto lg:text-xl sm:text-lg text-white ml-32 lg:ml-16 sm:ml-4">Welcome <span className='font-medium lg:text-2xl sm:text-xl'>Archie</span></p>
-            <div className="my-auto h-6 w-6 sm:h-8 sm:w-8 sm:ml-4 text-center rounded-full flex flex-column justify-center bg-orange-300">
+            <p className="hidden sm:block my-auto lg:text-xl text-md text-white lg:ml-12 sm:ml-4">Welcome <span className='font-medium lg:text-2xl text-md'>Archie</span></p>
+            <div className="hidden sm:block my-auto h-6 w-6 sm:h-8 sm:w-8 ml-1 text-center rounded-full flex flex-col justify-center bg-orange-300">
                 <IoMdArrowDropdown className='text-xl sm:text-2xl my-auto text-white font-light'/>
             </div>
+            
+            <div className='flex flex-col justify-end sm:hidden my-auto ml-20'>
+                <div>
+                    <FiMenu className=" text-white text-3xl font-medium"/>
+                </div>
+                <div>
+                    <ul>
+                        <li>User Profile</li>
+                        <li>Match</li>
+                        <li>Message</li>
+                    </ul>
+                </div>
+            </div>
+            
         </nav>
     )
 }
--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,47 +1,48 @@
 // import modules
 import React from "react";
+import '../css/UserProfile.css';
 
 export default function UserProfile() {
     let user_profile = true
     if (user_profile) {
         return (
             <div>
-                <h1 className='font-medium text-5xl mt-8'>Build User Profile</h1>
-                <form>
-                    <label for='name'>Name:</label>
-                    <input type="text" name="name"/>
-                    <label for='age'>Age:</label>
-                    <input type="number" name="age"/>
-                    <label for='studyYear'>Study year:</label>
-                    <input type="number" name="studyYear"/>
-                    <label for='profilePic'>Profile picture:</label>
-                    <input type="file" name="profilePic"/>
-                    <label for='department'>Department:</label>
-                    <input type="text" name="department"/>
-                    <label for='subject1'>Subject 1:</label>
-                    <input type='text' name='subject1'/>
-                    <label for='subject2'>Subject 2:</label>
-                    <input type='text' name='subject2'/>
-                    <label for='studyingStyle'>Studying style:</label>
-                    <select name='studyingStyle'>
+                <h1 className='font-medium text-5xl mt-8 sm:ml-4 lg:ml-12'>Build User Profile</h1>
+                <form className='profile-form mt-4 flex flex-col'>
+                    <label className='text-2xl font-light ml-8 lg:ml-20' for='name'>Name</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type="text" name="name"/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='age'>Age</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type="number" name="age"/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='studyYear'>Study year</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type="number" name="studyYear"/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='profilePic'>Profile picture</label>
+                    <input className='ml-8 lg:ml-20' type="file" name="profilePic"/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='department'>Department</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type="text" name="department"/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='subject1'>Subject 1</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type='text' name='subject1'/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='subject2'>Subject 2</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type='text' name='subject2'/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='studyingStyle'>Studying style</label>
+                    <select className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' name='studyingStyle'>
                         <option value='individual'>Quiet/Individual work</option>
                         <option value='group'>Discussion/Group work</option>
                     </select>
-                    <label for='description'>Description of yourself:</label>
-                    <input type='text' name='description'/>
-                    <label for='status'>Status:</label>
-                    <select name='status'>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='description'>Description of yourself</label>
+                    <textarea className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 rounded-sm text-xl pl-6' rows='4' name='description'/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='status'>Status</label>
+                    <select className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' name='status'>
                         <option value='partner'>Look for a long term study partner</option>
                         <option value='group'>Look for a study group</option>
                         <option value='homework'>Look for someone to help with homework</option>
                         <option value='review'>Look for some to review class material with</option>
                         <option value='test'>Look for test review partner</option>
                     </select>
-                    <label for='availability'>Availability (describe in words):</label>
-                    <input type='text' name='availability'/>
-                    <label for='location'>Location</label>
-                    <input type='text' name='location'/>
-                    <input type='submit' value='Post'/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='availability'>Availability (describe in words)</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type='text' name='availability'/>
+                    <label className='text-2xl font-light ml-8 lg:ml-20 mt-2' for='location'>Location</label>
+                    <input className='ml-8 lg:ml-20 border border-black w-72 sm:w-96 h-8 lg:h-12 rounded-sm text-xl pl-6' type='text' name='location'/>
+                    <input className='w-16 h-8 bg-red-500 hover:bg-red-700 rounded-sm ml-8 lg:ml-20 text-white mt-6 mb-4' type='submit' value='Post'/>
                 </form>
             </div>
         )
