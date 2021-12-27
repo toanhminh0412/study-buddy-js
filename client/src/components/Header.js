2b94c520134590e39871fd202f8f0985a0badaed
Minh To <toanhminh0412@gmail.com>
2021-12-27 01:23:38 -0800

    minor changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -29,7 +29,7 @@ export default function Header() {
                 <div className="ml-2 my-auto w-1/2 text-center 2xl:ml-40 lg:ml-16 sm:ml-2">
                     <Link to='/user-profile' className='hidden sm:inline-block ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white hover:text-amber-400'>User Profile</Link>
                     <Link to='/' className='hidden sm:inline-block ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white hover:text-amber-400'>Find a match</Link>
-                    <Link to='/' className='hidden sm:inline-block ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white hover:text-amber-400'>Message</Link>
+                    <Link to='/message' className='hidden sm:inline-block ml-2 2xl:ml-32 lg:ml-12 sm:ml-4 2xl:text-2xl lg:text-xl text-white hover:text-amber-400'>Message</Link>
                 </div>
                 <p className="hidden sm:block my-auto lg:text-xl text-md text-white lg:ml-12 sm:ml-4">Welcome <span className='font-medium lg:text-2xl text-md'>Archie</span></p>
 
