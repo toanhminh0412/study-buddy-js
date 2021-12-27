0bfed8e8e9919d120042769a8c794a0d5eb4fcd4
Minh To <toanhminh0412@gmail.com>
2021-12-27 01:58:39 -0800

    minor changes

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -20,7 +20,7 @@ export default function Dropdown() {
     return (
         <div className='bg-red-500 w-28 lg:w-40 right-0 absolute'>
             <ul className='flex flex-col text-center'>
-                <Link to='/' className="sm:hidden text-white text-xl h-10 border-b border-t flex flex-col justify-center hover:text-amber-300">Profile</Link>
+                <Link to='/user-profile' className="sm:hidden text-white text-xl h-10 border-b border-t flex flex-col justify-center hover:text-amber-300">Profile</Link>
                 <Link to='/' className="sm:hidden text-white text-xl h-10 border-b flex flex-col justify-center hover:text-amber-300">Match</Link>
                 <Link to='/' className="sm:hidden text-white text-xl h-10 border-b flex flex-col justify-center hover:text-amber-300">Message</Link>
                 <Link to='/' className="text-white text-xl h-10 border-b border-t flex flex-col justify-center hover:text-amber-300">Option</Link>
